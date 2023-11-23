a07c2091908e43b182b4c9b72c3d27fc2da8591a
stoneneedle <stoneneedle@gmail.com>
2023-11-23 13:48:09 -0500

    Modified BSTs.

--- DSAs/trees/bst_mine.py
+++ DSAs/trees/bst_mine.py
@@ -7,5 +7,118 @@ class TreeNode:
     def __str__(self):
         return str(self.val) + ' ' + str(self.left if self.left else '') + str(self.right if self.right else '')
 
-t = TreeNode(1)
-print(t.val)
+
+### Search, insert, minValue, and remove
+
+def search(root, target):
+    if not root:
+        return False
+
+    if target > root.val:
+        return search(root.right, target)
+    elif target < root.val:
+        return search(root.left, target)
+    else:
+        return True
+
+# Insert a new node and return the root of the BST.
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+    return root
+
+
+# Return the minimum value node of the BST.
+def minValueNode(root):
+    curr = root
+    while curr and curr.left:
+        curr = curr.left
+    return curr
+
+
+# Remove a node and return the root of the BST.
+def remove(root, val):
+    if not root:
+        return None
+
+    if val > root.val:
+        root.right = remove(root.right, val)
+    elif val < root.val:
+        root.left = remove(root.left, val)
+    else:
+        if not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
+        else:
+            minNode = minValueNode(root.right)
+            root.val = minNode.val
+            root.right = remove(root.right, minNode.val)
+    return root
+
+### Tree traversal (DFS)
+
+def inorder(root):
+    if not root:
+        return
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
+
+### BFS
+
+from collections import deque
+
+def bfs(root):
+    queue = deque()
+
+    if root:
+        queue.append(root)
+
+    level = 0
+    while len(queue) > 0:
+        print("level: ", level)
+        for i in range(len(queue)):
+            curr = queue.popleft()
+            print(curr.val)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+        level += 1
+
+
+
+
+
+### --------
+tn = TreeNode(4, TreeNode(3, TreeNode(2)), TreeNode(6, TreeNode(5), TreeNode(7)))
+
+
+# print("INORDER\n")
+# inorder(tn)
+# print("PREORDER\n")
+# preorder(tn)
+# print("POSTORDER\n")
+# postorder(tn)
+print("BFS\n")
+bfs(tn)
--- DSAs/trees/bst_neet.py
+++ DSAs/trees/bst_neet.py
@@ -1,3 +1,5 @@
+### Basic class definition
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -6,6 +8,8 @@ class TreeNode:
 
     def __str__(self):
         return str(self.val)
+
+### Search, insert, minValue, and remove
 
 def search(root, target):
     if not root:
@@ -57,6 +61,56 @@ def remove(root, val):
             root.val = minNode.val
             root.right = remove(root.right, minNode.val)
     return root
+
+### Tree traversal (DFS)
+
+def inorder(root):
+    if not root:
+        return
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
+
+### BFS
+
+from collections import deque
+
+def bfs(root):
+    queue = deque()
+
+    if root:
+        queue.append(root)
+
+    level = 0
+    while len(queue) > 0:
+        print("level: ", level)
+        for i in range(len(queue)):
+            curr = queue.popleft()
+            print(curr.val)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+        level += 1
+
+
+# -----
+
+
 
 bst = TreeNode(4)
 insert(bst, 2)
