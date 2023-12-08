ed3def26a57daad054bf43a271e425b9b832d3ea
stoneneedle <stoneneedle@gmail.com>
2023-12-07 19:03:31 -0500

    Modified heap_mine.py.

--- DSAs/heap/heap_mine.py
+++ DSAs/heap/heap_mine.py
@@ -1,13 +1,20 @@
 class Heap:
     def __init__(self):
-        self.heap = []
-        self.heap.append(0)
+        self.heap = [0]
+
+    def push(self, val):
+        self.heap.append(val)
+        i = len(self.heap) - 1
+        while i > 1 and self.heap[i] < self.heap[i // 2]:
+            self.heap[i], self.heap[i//2] = self.heap[i//2],self.heap[i]
+            i = i // 2
 
     def __str__(self):
         return str(self.heap)
 
 h = Heap()
 h.heap = [0,14,19,16,21,26,19,68,65,30,None,None,None,None,None,None]
+h.push(2)
 
 print(h)
 
